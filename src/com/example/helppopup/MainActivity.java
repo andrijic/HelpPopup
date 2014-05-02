46312fd5ceeb07f2323d73e8fc2018c3181da957
andrijic <ivanandrijic@gmail.com>
2014-05-02 08:24:46 +0200

    aaa

--- src/com/example/helppopup/MainActivity.java
+++ src/com/example/helppopup/MainActivity.java
@@ -1,23 +1,23 @@
 package com.example.helppopup;
 
 import hr.andrijic.uslugehr.helppopup.HelpPopup;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
-	HelpPopup popup ;
-	HelpPopup popup2 ;
-	
+	HelpPopup popup;
+	HelpPopup popup2;
+	HelpPopup popup3;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,28 +27,41 @@ public class MainActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-		
-		findViewById(R.id.container).post(new Runnable() {
-			
+
+		Button button = (Button) findViewById(R.id.testId2);
+		button.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				popup.setText("ccc 1").setPosition(150, 100);
+				popup2.setText("ccc 2").setPosition(150, 50);
+				popup3.removeAllViews();
+			}
+		});
+
+		final ViewGroup viewGroup = (ViewGroup) findViewById(android.R.id.content);
+		viewGroup.post(new Runnable() {
 			@Override
 			public void run() {
-				popup = new HelpPopup(MainActivity.this, findViewById(R.id.container))
-					.setText("ovo je tekst 1\n nova linij")
-					.setColor(Color.BLUE)
-					.setTextColor(Color.RED)
-					.setPosition(100, 100);
-				
-				popup2 = new HelpPopup(MainActivity.this, findViewById(R.id.container))
-				.setText("ovo je tekst 2\n nova linij")
-				.setColor(Color.BLUE)
-				.setTextColor(Color.RED)
-				.setPosition(100, 300)
-				.upArrowIsVisible(false);
+				popup = new HelpPopup(MainActivity.this)
+						.attachToViewGroup(viewGroup).setText("ovo je tekst 1")
+						.setColor(Color.BLUE).setTextColor(Color.RED)
+						.downArrowIsVisible(false)
+						.positionToBottomOfTargetView(findViewById(R.id.testId2));
+
+				popup2 = new HelpPopup(MainActivity.this)
+						.attachToViewGroup(viewGroup).setText("ovo je tekst 2")
+						.setColor(Color.CYAN).setTextColor(Color.RED)
+						.setPosition(100, 200).upArrowIsVisible(false);
+
+				popup3 = new HelpPopup(MainActivity.this)
+						.attachToViewGroup(viewGroup).setText("ovo je tekst 3")
+						.setColor(Color.GRAY).setTextColor(Color.RED)
+						.setPosition(150, 300).upArrowIsVisible(false);
+
 			}
 		});
 	}
-	
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- src/hr/andrijic/uslugehr/helppopup/HelpPopup.java
+++ src/hr/andrijic/uslugehr/helppopup/HelpPopup.java
@@ -2,16 +2,19 @@ package hr.andrijic.uslugehr.helppopup;
 
 
 import com.example.helppopup.R;
+import com.nineoldandroids.view.ViewHelper;
 
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
+import android.support.v7.internal.widget.ActionBarOverlayLayout;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver.OnPreDrawListener;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -21,31 +24,70 @@ import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class HelpPopup{
+public class HelpPopup extends LinearLayout implements OnPreDrawListener{
 	
 	private View view = null;
 	private TextView tv;
 	private ImageView upArrow;
 	private ImageView downArrow;
 	private FrameLayout contentHolder;
-	private RelativeLayout root;
+	private View refPositionView;
+	private int refPositionGravity;
 	
-	public HelpPopup(final Activity context, View rootView) {
+	public HelpPopup(final Activity context) {
 //		final PopupWindow window = new PopupWindow(context);
 //		window.setOutsideTouchable(true);
-		
+		super(context);
 		
 		LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		view = inflater.inflate(R.layout.popup_help, (ViewGroup)rootView.getParent(), true);
+		view = inflater.inflate(R.layout.popup_help, this, true);
 		tv = (TextView)view.findViewById(R.id.tooltip_contenttv);
 		upArrow = (ImageView)view.findViewById(R.id.tooltip_pointer_up);
 		downArrow = (ImageView)view.findViewById(R.id.tooltip_pointer_down);
 		contentHolder = (FrameLayout)view.findViewById(R.id.tooltip_contentholder);
-		root = (RelativeLayout)view.findViewById(R.id.tooltip_root);
+		//root = (RelativeLayout)view.findViewById(R.id.tooltip_root);
+		
+		getViewTreeObserver().addOnPreDrawListener(this);
 	}
 	
-	public HelpPopup setText(String text){
-		tv.setText(text);
+	public HelpPopup attachToViewGroup(ViewGroup viewGroup){
+		viewGroup.addView(this);
+		return this;
+	}
+	
+	public HelpPopup positionToBottomOfTargetView(View targetView){
+		refPositionView = targetView;
+		refPositionGravity = Gravity.BOTTOM;
+		
+		return this;
+	}
+	
+	private void reposition(){
+		if(refPositionView != null && refPositionGravity == Gravity.BOTTOM){
+			float x = ViewHelper.getX(refPositionView);
+			float y = ViewHelper.getY(refPositionView) + refPositionView.getHeight() + 5;
+			
+			x -= (refPositionView.getWidth() - contentHolder.getWidth())/2;
+			float x_arrow = ViewHelper.getX(refPositionView) - (refPositionView.getWidth() - upArrow.getWidth())/2;
+			
+			ViewHelper.setX(upArrow, ViewHelper.getX(upArrow));
+//			downArrowIsVisible(false);
+//			upArrowIsVisible(true);
+			
+			setPosition(x, y);
+		}
+	}
+
+	
+	public HelpPopup setText(final String text){
+		tv.post(new Runnable() {
+			
+			@Override
+			public void run() {
+				tv.setText(text);
+				
+			}
+		});
 		
 		return this;
 	}
@@ -63,22 +105,26 @@ public class HelpPopup{
 	}
 	
 	public HelpPopup upArrowIsVisible(boolean isVisible){
-		upArrow.setVisibility(0);
+		upArrow.setVisibility(View.GONE);
 		return this;
 	}
 	
 	public HelpPopup downArrowIsVisible(boolean isVisible){
-		downArrow.setVisibility(1);
+		downArrow.setVisibility(View.GONE);
 		return this;
 	}
 	
-	public HelpPopup setPosition(int x, int y){
-		FrameLayout.LayoutParams params = (FrameLayout.LayoutParams)root.getLayoutParams();
-		
-		params.leftMargin = x;
-		params.topMargin = y;
-		
-		root.setLayoutParams(params);
+	
+	public HelpPopup setPosition(float x, float y){
+		ViewHelper.setX(this, x);
+		ViewHelper.setY(this, y);
 		return this;
+	}
+
+	@Override
+	public boolean onPreDraw() {
+		reposition();
+		getViewTreeObserver().removeOnPreDrawListener(this);
+		return true;
 	}
 }
